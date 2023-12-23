e7e0fa0ce3fa036e6edd834cb9aac3cd606f2956
Abdulquyum <ajumobiabdulquyum@gmail.com>
2023-12-23 15:46:42 +0100

    prints factors of a number

---
+++ rsa.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void factorize(char *buffer);
+
+/**
+ * main - Entry point
+ *
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	FILE *fptr = NULL;
+	size_t n = 0;
+	char *buffer = NULL;
+	char *file;
+
+	if (argc != 2)
+	{
+		perror("Error");
+	}
+
+	file = argv[1];
+	fptr = fopen(file, "r");
+
+	if (fptr == NULL)
+	{
+		perror("Error");
+		exit(1);
+	}
+
+	getline(&buffer, &n, fptr);
+
+	factorize(buffer);
+
+	fclose(fptr);
+	free(buffer);
+
+	return (0);
+}
+
+/**
+ * factorize - factorize number into two products
+ *
+ * @buffer: line to be factorized
+ *
+ * Return: Nothing
+ */
+void factorize(char *buffer)
+{
+	int i, num, a;
+
+	num = atoi(buffer);
+
+	a = 2;
+
+	for (i = 0; buffer[i] != '\0'; i++)
+	{
+		while (a < num)
+		{
+			if (num % a == 0)
+			{
+				printf("%d=%d*%d\n", num, num/a, a);
+				break;
+			}
+			a += 1;
+		}
+	}
+	
+	
+}
